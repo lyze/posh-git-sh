972ab3850a24f943d4dc7508abdec7cb81622ea2
Johan Dewe <johan@dewe.net>
2018-11-19 21:34:02 +0100

    Trim whitespace from osx bash wc output

--- git-prompt.sh
+++ git-prompt.sh
@@ -318,7 +318,7 @@ __posh_git_echo () {
         if $ShowStashState; then
             git rev-parse --verify refs/stash >/dev/null 2>&1 && hasStash=true
             if $ShowStashCount && $hasStash; then
-                $stashCount=$(git stash list | wc -l)
+                stashCount=$(git stash list | wc -l | tr -d '[:space:]')
             fi
         fi
         __posh_git_ps1_upstream_divergence
